ec4ad2f3458ca04e2e7345dd23148e3e3d8688ad
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2019-10-18 10:29:25 +0900

    add config singleton

--- eval_generated_text.py
+++ eval_generated_text.py
@@ -1,20 +1,42 @@
 import textstat
 import configparser
+from threading import Lock
+
+CONFIG_FILE = "./config.ini"
 
 
- DEFAULT_CONFIG_FILE = "./config.ini"
+class ConfigUtil:
+    
+    _instance = None
+    _lock = Lock() 
 
-class ConfigUtil():
-    def __init__(self):
-        self.config = configparser.ConfigParser()
-        self.config.read(DEFAULT_CONFIG_FILE)
+    def __new__(cls):
+        raise NotImplementedError('Cannot initialize via Constructor')
 
-    def getConfig(self) :
-        return self.config
+    @classmethod
+    def __internal_new__(cls):
+        return super().__new__(cls)
+
+    @classmethod
+    def get_instance(cls):
+        if not cls._instance:
+            with cls._lock:
+                if not cls._instance:
+                    cls._instance = cls.__internal_new__()
+                    cls._instance.config = configparser.ConfigParser()
+                    cls._instance.config.read(CONFIG_FILE)
+        return cls._instance
 
 
-# ARIで評価
-textstat.automated_readability_index("")
+# # ARIで評価
+# textstat.automated_readability_index("")
+# configUtil = ConfigUtil()
+# pconfig = configUtil.getConfig()
 
+
+if __name__ == '__main__':
+    config = ConfigUtil.get_instance().config
+    
+    print(config.get('Paths','CsvPath'))
 
 
