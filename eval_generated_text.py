7eb18a42708dd58877b549156c391a99bcadbfc0
YusukeSuzuki1213 <cs16047@s.inf.shizuoka.ac.jp>
2019-10-22 19:06:53 +0900

    fix output column

--- eval_generated_text.py
+++ eval_generated_text.py
@@ -32,21 +32,28 @@ class ConfigUtil:
 def create_csv(config):
     csv_path = config.get('Paths','CsvPath')
     result_path = config.get('Paths','ResultPath')
+    csv_name = config.get('Paths', 'CsvName')
     result_list = [] # タプル: (読み込んだcsvの行番号, 生成された文のARI, 生成された文, 生成された文の日本語訳)
-    csv_title = ("読み込んだcsvの行番号", "生成された文のARI", "生成された文", "生成された文の日本語訳")
+    csv_title = ("読み込んだcsvの行番号", "word1", "word2", "FN", "word1 FE","word2 FE","生成された文のARI", "生成された文", "生成された文の日本語訳")
+    words = csv_name.split("_") # ['water','pen.csv']
+    word1 = words[0] # 'water'
+    word2 = words[1].split(".")[0] # 'pen'
 
     with open(csv_path, 'r') as f:
         for i, row in enumerate(csv.reader(f)):
             current_row_generated_sentences = row[2].split('\n')
+            fn = row[3]
+            word1_fe = row[4]
+            word2_fe = row[5]
             for sentence  in current_row_generated_sentences:
                 if len(sentence) != 0:
                     result_list.append(
                         #(i,textstat.automated_readability_index(sentence),sentence, Translator().translate(sentence, dest = 'ja').text)
-                        (i,textstat.automated_readability_index(sentence),sentence, "リクエスト制限")
+                        (i, word1, word2, fn, word1_fe, word2_fe, textstat.automated_readability_index(sentence),sentence, "リクエスト制限")
                     )
             print(i)
-
-    result_list.sort(key=lambda tup: tup[1]) # ARIでソート
+    del result_list[0]
+    result_list.sort(key=lambda tup: tup[6]) # ARIでソート
     
     with open(result_path, 'w') as f:
         writer = csv.writer(f)
